bfb4a2dcc4d3b707c7cc5279e512387e46a0d552
TYAGIVISHESH999 <tyagivishesh23@gmail.com>
2024-12-06 14:46:05 +0530

    LARGEST BETWEEN_THREE_NUMBERS.cpp

---
+++ LARGEST BETWEEN_THREE_NUMBERS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout<<"enter three numbers  "<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if((a>b)&&(a>c)){
+        cout<<"largest number is ";
+        cout<<a;
+    }
+    else if((b>a)&&(b>c)){
+        cout<<"largest number is ";
+        cout<<b;
+    }
+    else{
+        cout<<"largest number is ";
+        cout<<c;
+    }
+   
