fcb9dea2c0a987eba79ca767c934b9b12d76e6c1
TYAGIVISHESH999 <tyagivishesh23@gmail.com>
2024-12-09 14:36:23 +0530

    data abstraction.cpp

---
+++ data abstraction.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int a=3;
+    int b=4;
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of b is"<<a<<endl;
+    cout<<"after swapping is"<<endl;
+    swap(a,b);
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of b is "<<b<<endl;
+    int result=pow(a,b);
+    cout<<"the result is "<<result<<endl;
+    
+
+    return 0;
+}
